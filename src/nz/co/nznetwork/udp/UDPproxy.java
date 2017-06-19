974b31656630ad297b5378ac2e24a51119cf954b
sorifiend <sorifiend@hotmail.com>
2017-06-19 22:06:57 +1200

    Update UDPproxy.java

--- src/nz/co/nznetwork/udp/UDPproxy.java
+++ src/nz/co/nznetwork/udp/UDPproxy.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nz.co.nznetwork.udp;
 
 import java.net.*;
@@ -14,6 +10,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ *
+ * @author sorifiend
+ */
 public class UDPproxy
 {
   static final int CONNECTION_TIMEOUT = 60000;
